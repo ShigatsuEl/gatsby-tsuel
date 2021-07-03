0a914a5c09bf44b10ad59a0a398c8c8fc040e50f
MinChan Lee <shigatsu970704@gmail.com>
2021-07-03 18:02:23 +0900

    [Set] Gatsby Transformer Remark & First Test Post

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '@src/components/layout';
+import { BlogAllMarkDownRemark } from '@src/types/graphql';
 
-const BlogPage = () => {
+interface BlogPageProps {
+  data: BlogAllMarkDownRemark;
+}
+
+const BlogPage = ({ data }: BlogPageProps) => {
   return (
     <Layout>
       <h1>Blog Page</h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      <section>
+        <article>
+          <ul>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <li key={node.id}>
+                <h3>{node.frontmatter?.title}</h3>
+                <h4>{node.frontmatter?.date}</h4>
+                <p>{node.excerpt}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </section>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query BlogAllMarkDownRemark {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
 
 export default BlogPage;
