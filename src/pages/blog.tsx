6fb64afd7e65d9e9e9197340464421dfc3db3b70
MinChan Lee <shigatsu970704@gmail.com>
2021-07-11 18:26:56 +0900

    [Add] Gatsby Creating Category Search for Blog Posts

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,32 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from '@reach/router';
 import { graphql, Link } from 'gatsby';
+import queryString from 'query-string';
 
 import Layout from '@src/components/layout';
 import { BlogAllMarkDownRemark } from '@src/types/graphql';
+import { useState } from 'react';
 
 interface BlogPageProps {
   data: BlogAllMarkDownRemark;
   pageContext: any;
 }
 
-const BlogPage = ({ pageContext, data }: BlogPageProps) => {
-  console.log(pageContext);
+const BlogPage = ({ data }: BlogPageProps) => {
+  const [categoryName, setCategoryName] = useState('');
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.search === '') {
+      setCategoryName('all');
+    } else {
+      setCategoryName(queryString.parse(location.search).category as string);
+    }
+  }, [location]);
+
   return (
     <Layout>
-      <h1>Blog Page</h1>
-      <h4>{data.allMdx.totalCount} Posts</h4>
-      <section>
-        <article>
+      <header>
+        <h1>Blog Page</h1>
+        <nav>
           <ul>
-            {data.allMdx.edges.map(({ node }) => (
-              <li key={node.id}>
-                <Link to={`${node.fields?.slug}` ?? '/404/'}>
-                  <h3>{node.frontmatter?.title}</h3>
-                  <h4>{node.frontmatter?.date}</h4>
-                  <p>{node.excerpt}</p>
-                </Link>
+            {data.allMdx.group.map((category) => (
+              <li key={category.tag}>
+                <Link
+                  to={`/blog/?category=${category.tag}`}
+                >{`${category.tag} (${category.totalCount})`}</Link>
               </li>
             ))}
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <article>
+          <h4>{data.allMdx.totalCount} Posts</h4>
+          <ul>
+            {data.allMdx.edges
+              .filter(({ node }) => node.frontmatter?.tags?.some((tag) => tag === categoryName))
+              .map(({ node }) => (
+                <li key={node.id}>
+                  <Link to={`${node.fields?.slug}` ?? '/404/'}>
+                    <h3>{node.frontmatter?.title}</h3>
+                    <h4>{node.frontmatter?.date}</h4>
+                    <p>{node.excerpt}</p>
+                  </Link>
+                </li>
+              ))}
           </ul>
         </article>
       </section>
@@ -48,6 +76,7 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "MMMM DD, YYYY")
+            tags
           }
           fields {
             slug
