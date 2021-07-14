f4abee8b3bfdc27c59f9b382f9c1608f3cce1aa6
MinChan Lee <shigatsu970704@gmail.com>
2021-07-14 22:18:18 +0900

    [Add] Highlight Code Block CSS At global-styled.ts

--- src/styles/global-styled.ts
+++ src/styles/global-styled.ts
@@ -55,6 +55,10 @@ export const GlobalStyle = createGlobalStyle`
   overflow: initial;
 }
 
+.line-numbers-rows span::before {
+  color: #e0e0e0;
+}
+
 .command-line-prompt > span:before {
   color: #999;
   content: " ";
@@ -74,5 +78,118 @@ export const GlobalStyle = createGlobalStyle`
 
 .command-line-prompt > span[data-prompt]:before {
   content: attr(data-prompt);
+}
+
+.command-line-prompt > span::before {
+  color: #e0e0e0;
+}
+
+code[class*="language-"],
+pre[class*="language-"] {
+	color: #e0e0e0;
+	background: none;
+	font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
+	text-align: left;
+	white-space: pre;
+	word-spacing: normal;
+	word-break: normal;
+	word-wrap: normal;
+	line-height: 1.5;
+
+	-moz-tab-size: 4;
+	-o-tab-size: 4;
+	tab-size: 4;
+
+	-webkit-hyphens: none;
+	-moz-hyphens: none;
+	-ms-hyphens: none;
+	hyphens: none;
+
+}
+
+/* Inline code */
+:not(pre) > code[class*='language-'] {
+  padding: .11em .3em;
+  margin: 0 .1em;
+  border-radius: .3em;
+  white-space: normal;
+  background: #fffbfe;
+  color: #da3a6a;
+  border: 1.2px solid #da3a6a;
+}
+
+/* token set */
+.token.comment,
+.token.block-comment,
+.token.prolog,
+.token.doctype,
+.token.cdata {
+  color: #616161;
+}
+
+.token.punctuation {
+  color: #e0e0e0;
+}
+
+.token.tag,
+.token.attr-name,
+.token.namespace,
+.token.deleted {
+  color: #e2777a;
+}
+
+.token.function-name {
+  color: #6196cc;
+}
+
+.token.boolean,
+.token.number,
+.token.function {
+  color: #ff9100;
+}
+
+.token.property,
+.token.class-name,
+.token.constant,
+.token.symbol {
+  color: #ffff00;
+}
+
+.token.selector,
+.token.important,
+.token.atrule,
+.token.keyword,
+.token.builtin {
+  color: #b388ff;
+}
+
+.token.string,
+.token.char,
+.token.attr-value,
+.token.regex,
+.token.variable {
+  color: #00e676;
+}
+
+.token.operator,
+.token.entity,
+.token.url {
+  color: #2bccca;
+}
+
+.token.important,
+.token.bold {
+  font-weight: bold;
+}
+.token.italic {
+  font-style: italic;
+}
+
+.token.entity {
+  cursor: help;
+}
+
+.token.inserted {
+  color: green;
 }
 `;
